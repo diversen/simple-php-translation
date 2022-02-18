bef3ec830609efd7751b5b3adc83520b86b91c0b
Dennis Iversen <dennis.iversen@gmail.com>
2022-02-18 09:48:28 +0100

    translate

--- app/lang/da/language.php
+++ app/lang/da/language.php
@@ -2,6 +2,6 @@
 
 $LANG = array();
 
-$LANG['Here is a test'] = 'Here is a test';
-$LANG['User with ID <span class="notranslate">{ID}</span> has been locked!'] = 'User with ID <span class="notranslate">{ID}</span> has been locked!';
+$LANG['Here is a test'] = 'Her er en test';
+$LANG['User with ID <span class="notranslate">{ID}</span> has been locked!'] = 'Bruger med ID <span class="notranslate">{ID}</span> er blevet låst!';
 $LANG['User with ID <span class="notranslate">{ID}</span> and username <span class="notranslate">{username}</span> has been locked !'] = 'Bruger med ID <span class="notranslate">{ID}</span> og brugernavn <span class="notranslate">{username}</span> er blevet låst!';
