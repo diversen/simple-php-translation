717b2fd48ca4bf0edf32b1bca1e2fd618f032e77
Dennis Iversen <dennis.iversen@gmail.com>
2022-04-13 13:20:21 +0200

    allow for multiple method names to search from. Then it will be possible to extract translations from both e.g. php and js files.

--- src/Translate/Extractor.php
+++ src/Translate/Extractor.php
@@ -2,20 +2,36 @@
 
 namespace Diversen\Translate;
 
-class Extractor {
-    
+class Extractor
+{
+
     var $dirs = [];
-    /** file where translation will be placed */
-    var $translateFile = 'language.php';
-    /** translation dir where translation files will be placed */
-    var $translateDir = 'lang';
-    /** name of var inside translateFile LANG */
-    var $translateAryName = 'LANG';
-    /** pattern to search for */
-    var $extractMethodName = 'Lang::translate';
-    /** default language. This is default language we extract from */
-    var $defaultLanguage ='en';
     
+    /** 
+     * File where translation strings will be placed 
+     */
+    var $translateFile = 'language.php';
+
+    /** 
+     * translation dir where translation files will be placed 
+     * */
+    var $translateDir = 'lang';
+
+    /** 
+     * name of var inside translateFile LANG 
+     */
+    var $translateAryName = 'LANG';
+
+    /** 
+     * Method names to search for 
+     */
+    var $extractMethodName = ['Lang::translate', 'Lang.translate'];
+
+    /** 
+     * default language. This is default language we extract from 
+     */
+    var $defaultLanguage = 'en';
+
     /**
      * attach all dirs inside a dir to $this->dirs
      * e.g. modules/* will set the following dirs
@@ -23,56 +39,67 @@ class Extractor {
      * /modules/blog
      * @param string $path modules/*
      */
-    public function setDirsInsideDir ($path) {
+    public function setDirsInsideDir($path)
+    {
         $dirs = self::getDirsGlob($path);
         $this->addDirs($dirs);
     }
-    
+
     /**
      * add some dirs
      * @param array $dirs
      */
-    private function addDirs ($dirs) { 
-        foreach($dirs as $dir) {
+    private function addDirs($dirs)
+    {
+        foreach ($dirs as $dir) {
             $this->dirs[] = $dir;
         }
     }
-    
+
     /**
      * set a single path for e.g. a vendor lib
      * @param string $path
      */
-    public function setSingleDir ($path) {
+    public function setSingleDir($path)
+    {
         $this->dirs[] = $path;
     }
-    
-    /**
-     * search for strings which we should extract
-     * @param type $str
-     * @return type
-     */
-    public function search ($str = '') {
-        $method_name = $this->extractMethodName;
 
-        // find all strings matching inside $this->extractMethodName
-        // by default we look for Lang::translate calls
-        
+    private function searchSingleExtractMethod($str, $method_name) {
+
         // search for strings inside ''
         $search = "/$method_name\('([^']+)'/s";
-        
         preg_match_all($search, $str, $out);
         $strings = $out[1];
         $strings = array_unique($strings);
 
         // search for strings inside ""
         $search = '/' . $method_name . '\("([^"]+)"/s';
-
-        preg_match_all($search, $str, $out2);
-        $strings2 = $out2[1];
+        preg_match_all($search, $str, $out);
+        $strings2 = $out[1];
         $strings = array_merge($strings, $strings2);
+
         return $strings;
     }
-    
+
+    /**
+     * Search a string for strings that should be translated
+     * @param string $str
+     * @return array $strings 
+     */
+    public function search($str = '')
+    {
+        $strings = [];
+
+        foreach($this->extractMethodName as $method_name) {
+            $strings_extracted = $this->searchSingleExtractMethod($str, $method_name);
+        }
+
+        $strings = array_merge($strings, $strings_extracted);
+        return $strings;
+        
+    }
+
 
     /**
      * get a line of the translation file with correct quotes
@@ -81,7 +108,8 @@ class Extractor {
      * @param string $value
      * @return string $str
      */
-    public function setCorrectQuotes($key, $value = '') {
+    public function setCorrectQuotes($key, $value = '')
+    {
 
         $str = '';
         if (empty($value)) {
@@ -92,64 +120,68 @@ class Extractor {
         // in order to know how to write out the array
         $apo_key = strpos($key, "'");
         if ($apo_key) {
-            $str.= "\$" . $this->translateAryName . '["' . $key . '"] = ';
+            $str .= "\$" . $this->translateAryName . '["' . $key . '"] = ';
         } else {
-            $str.= "\$" . $this->translateAryName . "['$key'] = ";
+            $str .= "\$" . $this->translateAryName . "['$key'] = ";
         }
 
         // and also for the value
         $apo_val = strpos($value, "'");
         if ($apo_val) {
-            $str.= '"' . $value . '";' . "\n";
+            $str .= '"' . $value . '";' . "\n";
         } else {
-            $str.= "'$value';" . "\n";
+            $str .= "'$value';" . "\n";
         }
         return $str;
     }
-    
+
     /**
      * check if prim mime type is text
      * @param string $file
      * @return boolean $res true if text else false
      */
-    private function isText($file) {
+    private function isText($file)
+    {
         $prim_mime = self::getPrimMime($file);
         if ($prim_mime == 'text') {
             return true;
         }
         return false;
     }
-    
+
     /**
      * get language dir name to be used in source dir
      * @param string $dir
      * @return string $dir
      */
-    public function getLanguageDirFromDir ($dir) {
-        return 
-                $dir . '/' . 
-                $this->translateDir . '/' . 
-                $this->defaultLanguage;
+    public function getLanguageDirFromDir($dir)
+    {
+        return
+            $dir . '/' .
+            $this->translateDir . '/' .
+            $this->defaultLanguage;
     }
-    
+
     /**
      * get language file name from dir
      * @param string $dir
      * @return string $file
      */
-    public function getLanguageFileFromDir($dir) {
+    public function getLanguageFileFromDir($dir)
+    {
         return
-                $this->getLanguageDirFromDir($dir) . '/' .
-                $this->translateFile;
+            $this->getLanguageDirFromDir($dir) . '/' .
+            $this->translateFile;
     }
-    
+
     /**
      * create translation file
      * @param string $dir
      * @param string $str translation string
      */
-    public function createFile ($dir, $str) {
-        
+    public function createFile($dir, $str)
+    {
+
         $save_dir = $this->getLanguageDirFromDir($dir);
         if (!file_exists($save_dir)) {
             $res = mkdir($save_dir, 0755, true);
@@ -160,7 +192,6 @@ class Extractor {
         }
         $file = $this->getLanguageFileFromDir($dir);
         file_put_contents($file, $str);
-  
     }
 
     /**
@@ -169,7 +200,8 @@ class Extractor {
      * @param string $pattern a given fnmatch() pattern
      * return array $ary an array with the files found. 
      */
-    public static function getFileListRecursive($start_dir, $pattern = null) {
+    public static function getFileListRecursive($start_dir, $pattern = null)
+    {
         $files = array();
         if (is_dir($start_dir)) {
             $fh = opendir($start_dir);
@@ -206,15 +238,16 @@ class Extractor {
      *              'basename' => '/path/to/exists'
      * @return array $dirs 
      */
-    public static function getDirsGlob($path, $options = array()) {
+    public static function getDirsGlob($path, $options = array())
+    {
         if (file_exists($path)) {
             $it = new \DirectoryIterator($path);
-        } else {      
+        } else {
             error_log("$path does not exists in " . __FILE__ . ": " . __LINE__);
             return array();
         }
-        
-        $dirs = array ();
+
+        $dirs = array();
         foreach ($it as $file) {
             if ($file->isDir() && !$file->isDot()) {
                 $dirs[] = $it->getPathname();
@@ -233,7 +266,8 @@ class Extractor {
      * @param string $path
      * @return string $mime_type 
      */
-    public static function getMime($path) {
+    public static function getMime($path)
+    {
         $result = false;
         if (is_file($path) === true) {
             if (function_exists('finfo_open') === true) {
@@ -255,48 +289,50 @@ class Extractor {
      * @param string $file
      * @return string $mime
      */
-    public static function getPrimMime($file) {
+    public static function getPrimMime($file)
+    {
         $str = self::getMime($file);
         $ary = explode('/', $str);
         return $ary[0];
     }
-    
+
     /**
      * generates a PHP string with extracted translation
      * @param string $dir
      * @return string $translation_str
      */
-    public function createLangStrForPath ($dir) {
+    public function createLangStrForPath($dir)
+    {
         $file_list = self::getFileListRecursive($dir);
         asort($file_list);
 
         // compose a php file
         $translation_str = "<?php\n\n";
-        $translation_str.= "$" . $this->translateAryName . " = array();\n\n";
+        $translation_str .= "$" . $this->translateAryName . " = array();\n\n";
 
         foreach ($file_list as $file) {
             if (!$this->isText($file)) {
                 continue;
             }
-            
+
             $file_str = file_get_contents($file);
             $strings = $this->search($file_str);
-            
+
             // no strings we continue
             if (empty($strings)) {
                 continue;
             }
-            
-            $translation_str.= "\n// Translation of file $file\n\n";
+
+            $translation_str .= "\n// Translation of file $file\n\n";
 
             // and we add all strings in that file
             foreach ($strings as $trans) {
-                $translation_str.= $this->setCorrectQuotes($trans);
+                $translation_str .= $this->setCorrectQuotes($trans);
             }
-        }  
+        }
         return $translation_str;
     }
-    
+
     /**
      * Updates a PHP string with extracted translation
      * Updating means that any changes to the values of 
@@ -304,19 +340,20 @@ class Extractor {
      * @param string $dir
      * @return string $translation_str
      */
-    public function updateLangStrForPath ($dir) {
+    public function updateLangStrForPath($dir)
+    {
         $file_list = self::getFileListRecursive($dir);
         asort($file_list);
 
         // compose a php file
         $translation_str = "<?php\n\n";
-        $translation_str.= "$" . $this->translateAryName . " = array();\n";
-    
+        $translation_str .= "$" . $this->translateAryName . " = array();\n";
+
         // if lang file does not exists
         $lang_file = $this->getLanguageFileFromDir($dir);
         if (!file_exists($lang_file)) {
             return $this->createLangStrForPath($dir);
-        } 
+        }
 
         include $lang_file;
 
@@ -324,38 +361,38 @@ class Extractor {
         if (empty($original_ary)) {
             return $this->createLangStrForPath($dir);
         }
-        
+
         foreach ($file_list as $file) {
             if (!$this->isText($file)) {
                 continue;
             }
-            
+
             $file_str = file_get_contents($file);
             $strings = $this->search($file_str);
-            
+
             // no strings we continue
             if (empty($strings)) {
                 continue;
             }
-            
-            $translation_str.= "\n// Translation of file $file\n\n";
+
+            $translation_str .= "\n// Translation of file $file\n\n";
 
             // and we add all strings
-            foreach ($strings as $trans) { 
+            foreach ($strings as $trans) {
                 if (!isset($original_ary[$trans])) {
                     // new value
-                    $translation_str.=$this->setCorrectQuotes($trans);
+                    $translation_str .= $this->setCorrectQuotes($trans);
                 } else {
                     // keep old value
                     $old_value = $original_ary[$trans];
-                    $translation_str.= $this->setCorrectQuotes($trans, $old_value);
-                }                
+                    $translation_str .= $this->setCorrectQuotes($trans, $old_value);
+                }
             }
         }
         return $translation_str;
     }
-    
-        
+
+
     /**
      * generate a language file in a language dir
      * will overwrite previous translations
@@ -364,13 +401,14 @@ class Extractor {
      * 
      * @param type $lang
      */
-    public function generateLang () {
+    public function generateLang()
+    {
         foreach ($this->dirs as $dir) {
             $str = $this->createLangStrForPath($dir);
             $this->createFile($dir, $str);
         }
     }
-    
+
     /**
      * generate a language file in a language dir
      * will overwrite previous translations
@@ -379,7 +417,8 @@ class Extractor {
      * 
      * @param type $lang
      */
-    public function updateLang () {
+    public function updateLang()
+    {
         foreach ($this->dirs as $dir) {
             $str = $this->updateLangStrForPath($dir);
             $this->createFile($dir, $str);
